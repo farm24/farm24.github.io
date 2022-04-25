8f13a996b8e643747130abf1fd1f12b6067027d1
farm24 <77849362+farm24@users.noreply.github.com>
2022-04-25 12:03:12 -0500

    still broken

--- hi.js
+++ hi.js
@@ -1,7 +1,7 @@
 function loadDoc() {
   const xhttp = new XMLHttpRequest();
   xhttp.onload() = function() {
-    document.getElementById("d").innerHTML = this.response;
+    document.getElementById("d").innerHTML = this.responseText;
   }
   xhttp.open("GET", "hi.", true);
   xhttp.send();
