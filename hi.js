fcd721ef235fe46416dd0e8054d9a124c8f55d59
farm24 <77849362+farm24@users.noreply.github.com>
2022-04-25 10:49:29 -0500

    changed

--- hi.js
+++ hi.js
@@ -1,7 +1,7 @@
 function loadDoc() {
   const xhttp = new XMLHttpRequest();
   xhttp.onload() = function() {
-    document.getElementById("b").innerHTML = this.responseText;
+    document.getElementById("d").innerHTML = this.responseText;
   }
   xhttp.open("GET", "hi.txt", true);
   xhttp.send();
